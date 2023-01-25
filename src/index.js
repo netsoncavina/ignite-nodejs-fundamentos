c25daa307670822e68976c66e0d45b14474c657a
Netson Cavina <netsoncavina@gmail.com>
2023-01-24 21:44:13 -0300

    Tipos de parâmetros

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 /**
  * GET = Buscar
  * POST = Salvar
@@ -8,15 +9,30 @@ const app = express();
  * DELETE = Deletar
  * PATCH = Alteração específica
  */
+
+/**
+ * Tipos de parâmetros:
+ *
+ * Route Params => Identificar um recurso editar/deletar/buscar
+ * Query Params => Paginação, filtros, ordenação
+ * Body Params => Objetos para criação ou alteração de um registro
+ *
+ */
 app.get("/courses", (request, response) => {
+  const query = request.query;
+  console.log(query);
   return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post("/courses", (request, response) => {
+  const body = request.body;
+  console.log(body);
   return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put("/courses/:id", (request, response) => {
+  const { id } = request.params;
+  console.log(id);
   return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
