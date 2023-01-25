231920e57e68b2cb840294bee57149c0143641c2
Netson Cavina <netsoncavina@gmail.com>
2023-01-25 00:11:51 -0300

    get statement

--- src/index.js
+++ src/index.js
@@ -33,4 +33,10 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  return response.json(customer.json);
+});
+
 app.listen(3333);
