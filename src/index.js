d665fd5d570e863a25e2046d04e61cc8ffefc71a
Netson Cavina <netsoncavina@gmail.com>
2023-01-24 21:29:23 -0300

    utilizando métodos HTTP

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 const express = require("express");
 
 const app = express();
+/**
+ * GET = Buscar
+ * POST = Salvar
+ * PUT = Alterar
+ * DELETE = Deletar
+ * PATCH = Alteração específica
+ */
+app.get("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+app.post("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 4"]);
 });
 
 app.listen(3333);
