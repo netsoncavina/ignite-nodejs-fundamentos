120eec973c60051f7f1c314120f9795ee7342a6d
Netson Cavina <netsoncavina@gmail.com>
2023-01-25 00:20:40 -0300

    adição de middleware de validação de cpf

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ### Requisitos
 
 - [x] Deve ser possivel criar uma conta
-- [] Deve ser possivel buscar o extrato bancário do cliente
+- [x] Deve ser possivel buscar o extrato bancário do cliente
 - [] Deve ser possivel realizar um depósito
 - [] Deve ser possivel realizar um saque
 - [] Deve ser possivel buscar o extrato bancário do cliente por data
@@ -18,8 +18,8 @@
 ### Regras de negócio
 
 - [x] Não deve ser possivel cadastrar uma conta com CPF já existente
+- [x] Não deve ser possivel buscar extrato em uma conta não existente
 - [] Não deve ser possivel fazer depósito em uma conta não existente
-- [] Não deve ser possivel buscar extrato em uma conta não existente
 - [] Não deve ser possivel fazer saque em uma conta não existente
 - [] Não deve ser possivel excluir uma conta não existente
 - [] Não deve ser possivel fazer saque quando o saldo for insuficiente
--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ const app = express();
 app.use(express.json());
 
 const customers = [];
-/**
- *
- * cpf - string
- * name - string
- * id - uuid
- * statement []
- *
- */
+
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found!" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -33,13 +41,8 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/", (request, response) => {
-  const { cpf } = request.headers;
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found!" });
-  }
+app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
 
   return response.json(customer.json);
 });
